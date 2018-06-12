dc6c80b57fb777693998ce657a3cba19575bef4b
Milesh <milesh.jivan@gmail.com>
2018-06-12 23:10:29 +0200

    2018/06/12 23:09

--- app/src/main/java/com/example/mjivan/weatherforcasterv1/MapsActivity.kt
+++ app/src/main/java/com/example/mjivan/weatherforcasterv1/MapsActivity.kt
@@ -9,10 +9,21 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import android.support.v4.content.ContextCompat
+import android.support.v4.app.ActivityCompat
+import android.Manifest
+import android.widget.Toast
+import android.content.pm.PackageManager
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLngBounds
+
+//import javax.naming.Context
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    //adding permission request code
+    private val LOCATION_REQUEST_CODE=101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,10 +45,60 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        val zoomLevel = 16.0f //This goes up to 21
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        if(mMap !=null) {
+           // val CoarseLocationPermissions = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            val permission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+
+
+            if (permission == PackageManager.PERMISSION_GRANTED)
+            {
+                mMap?.isMyLocationEnabled = true;
+            }
+
+            else
+            {
+                requestPermission(Manifest.permission.ACCESS_FINE_LOCATION,
+                        LOCATION_REQUEST_CODE)
+
+            }
+
+        }
+        //Requesting App Permission
+
     }
+    private fun requestPermission(permissionType:String,requestCode:Int)
+    {
+        ActivityCompat.requestPermissions(this,arrayOf(permissionType),
+        requestCode)
+    }
+//Permission Handling
+  override fun onRequestPermissionsResult(requestCode:Int,permissions:Array<String>,grantResults:IntArray)
+    {
+        when(requestCode)
+        {
+            LOCATION_REQUEST_CODE ->
+            {
+             if(grantResults.isEmpty()|| grantResults[0]!=
+                     PackageManager.PERMISSION_GRANTED)
+             {
+                 Toast.makeText(this,
+                         "Sorry,we cannot display your location,please grant us permissions to do so :)",
+                         Toast.LENGTH_LONG).show()
+             }
+
+                else
+             {
+                 val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+                 mapFragment.getMapAsync(this)
+             }
+            }
+        }
+    }
+
+
+
+
 }
