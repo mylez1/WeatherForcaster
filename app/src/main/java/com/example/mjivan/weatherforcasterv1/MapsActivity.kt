1069b281e753c1d947329a8c3498541423d7ff07
DYNAMICDNA\MJivan <mjivan@dvt.co.za>
2018-06-13 15:16:55 +0200

    2018/06/12 3:24

--- app/src/main/java/com/example/mjivan/weatherforcasterv1/MapsActivity.kt
+++ app/src/main/java/com/example/mjivan/weatherforcasterv1/MapsActivity.kt
@@ -6,6 +6,11 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.widget.Toast
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GooglePlayServicesUtil
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationRequest
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -15,6 +20,13 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+val PERMISSION_REQUEST_CODE=101
+    val PLAY_SERVICE_RESOLUTION_REQUEST=100
+
+    //Variables
+    val mGoogleApiClient:GoogleApiClient?=null
+    val mLocationRequest:LocationRequest?=null
+
 
     private lateinit var mMap: GoogleMap
 
@@ -25,7 +37,61 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        requestPermission()
     }
+
+    private fun requestPermission() {
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED
+        && ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
+                {
+                    requestPermissions(arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION,android.Manifest.permission.ACCESS_FINE_LOCATION),PERMISSION_REQUEST_CODE)
+                }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode)
+        {
+            PERMISSION_REQUEST_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (checkPlayService()) {
+                        buildGoogleApiClient()
+                    }
+
+                }
+
+            }
+        }
+    }
+
+
+    private fun buildGoogleApiClient()
+    {
+mGoogleApiClient = GoogleApiClient.Builder(this)
+        .addConnectionCallbacks()
+    }
+    private fun checkPlayService(): Boolean
+    {
+        var resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this)
+        if(resultCode != ConnectionResult.SUCCESS)
+        {
+            if(GooglePlayServicesUtil.isUserRecoverableError(resultCode))
+            {
+                GooglePlayServicesUtil.getErrorDialog(resultCode,this,PLAY_SERVICE_RESOLUTION_REQUEST).show()
+            }
+
+            else
+            {
+                Toast.makeText(applicationContext,"This device is not supported", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+            return false
+        }
+    return true
+        }
+
+
+
+
 
     /**
      * Manipulates the map once available.
@@ -36,16 +102,16 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+    //override fun onMapReady(googleMap: GoogleMap) {
+       // mMap = googleMap
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    //    mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+    //    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15F))
-        googleMap.isMyLocationEnabled=true
+     //   googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15F))
+
 
 
 fun checkPermission(context: Context, permissionArray :Array<String>):Boolean
@@ -54,4 +120,6 @@ fun checkPermission(context: Context, permissionArray :Array<String>):Boolean
 }
 
     }
+
+
 }
